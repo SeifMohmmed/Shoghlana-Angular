c624ab662eec47ca870836696585a43f63d1023d
Seif Mohamed <seif.eissa7797@gmail.com>
2025-09-26 00:21:24 +0300

    Add Navbar Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,28 @@ import { FreelancersModule } from './freelancers/freelancers.module';
 import { NotFoundComponent } from './Shared/Components/not-found/not-found.component';
 import { FooterComponent } from './Shared/Components/footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './Shared/Components/navbar/navbar.component';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent, NotFoundComponent, FooterComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule, IdentityModule, FreelancersModule],
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarComponent,
+    NavbarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    IdentityModule,
+    FreelancersModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
 export class AppModule {}
