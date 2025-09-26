0bc962807041781d6a856f19fc5b0c32ef5b3bde
Seif Mohamed <seif.eissa7797@gmail.com>
2025-09-26 21:13:33 +0300

    Add Project Details Component

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -5,9 +5,10 @@ import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectComponent } from './project/project.component';
 import { ProjectSideBarComponent } from './project-side-bar/project-side-bar.component';
 import { FormsModule } from '@angular/forms';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
 
 @NgModule({
-  declarations: [ProjectComponent, ProjectSideBarComponent],
+  declarations: [ProjectComponent, ProjectSideBarComponent, ProjectDetailsComponent],
   imports: [CommonModule, ProjectRoutingModule, FormsModule],
 })
 export class ProjectModule {}
