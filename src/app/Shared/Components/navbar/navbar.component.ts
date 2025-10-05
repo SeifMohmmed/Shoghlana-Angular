34928a04dcc3a5fa3cbd8068bfe3f78192d3c828
Seif Mohamed <seif.eissa7797@gmail.com>
2025-10-05 23:37:39 +0300

    Add dropdown for messages and enhance navbar functionality

--- src/app/Shared/Components/navbar/navbar.component.ts
+++ src/app/Shared/Components/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { IdentityService } from '../../../identity/identity.service';
+import { ChatService } from '../../Services/chat.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   isLogged: boolean = false;
+  isOpen: boolean = false;
+  messages: any;
 
-  constructor(private identityService: IdentityService) {
+  constructor(
+    private identityService: IdentityService,
+    private chatService: ChatService
+  ) {
+    //using behavoir subject this code works with every change in dom
     identityService.userData.subscribe({
       next: () => {
         if (identityService.userData.getValue() !== null) {
@@ -19,6 +26,25 @@ export class NavbarComponent {
         }
       },
     });
+  }
+
+  ngOnInit(): void {
+    this.chatService.messages$.subscribe((res) => {
+      this.messages = res;
+      console.log(this.messages);
+    });
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown-container')) {
+      this.isOpen = false;
+    }
+  }
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
   }
 
   logout() {
